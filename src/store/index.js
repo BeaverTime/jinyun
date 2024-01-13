09fe9bffaf99b1b9b2d1b048fa14f784b8497ddf
wangtongliang <wangtongliang@wps.cn>
2024-01-13 16:53:33 +0800

    init

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,12 @@
 import { createStore } from "vuex";
+import UserStore from './users'
 
 export default createStore({
   state: {},
   getters: {},
   mutations: {},
   actions: {},
-  modules: {},
+  modules: {
+    UserStore
+  },
 });
---
+++ src/store/users.js
@@ -0,0 +1,11 @@
+/** @type {import('vuex').Module} */
+const UserStore = {
+    state:{
+        users:[]
+    },
+    actions: {
+        
+    },
+    namespaced: true
+}
+export default UserStore;
